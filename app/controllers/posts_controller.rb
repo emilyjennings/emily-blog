5fc0ff97f1f4dac5ca5058b6826e0454fbe8c1d7
github username <github email address>
2019-05-26 21:31:42 -0400

    put back so maybe heroku will work again

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -78,6 +78,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :created, :content, :category_id)
+      params.require(:post).permit(:title, :created, :content)
     end
 end
