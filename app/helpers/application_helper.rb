fbbd4e751ad701d168c52eae82103279f528c5f7
github username <github email address>
2019-06-04 22:16:30 -0400

    fixed date format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,31 +33,32 @@ module ApplicationHelper
 
   def convert_date(date)
     new_date = date.split("-")
-    if new_date[1] == '01'
-      "January #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '02'
-      "February #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '03'
-      "March #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '04'
-      "April #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '05'
-      "May #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '06'
-      "June #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '07'
-      "July #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '08'
-      "August #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '09'
-      "September #{new_date[2].split('')[1]}, #{new_date[0]}"
-    elsif new_date[1] == '10'
-      "October #{new_date[2]}, #{new_date[0]}"
-    elsif new_date[1] == '11'
-      "November #{new_date[2]}, #{new_date[0]}"
-    elsif new_date[1] == '12'
-      "December #{new_date[2]}, #{new_date[0]}"
-    end
+    day = new_date[2].split('')[0] == '0' ? new_date[2].split('')[1] : new_date[2]
+      if new_date[1] == '01'
+        "January #{day}, #{new_date[0]}"
+      elsif new_date[1] == '02'
+        "February #{day}, #{new_date[0]}"
+      elsif new_date[1] == '03'
+        "March #{day}, #{new_date[0]}"
+      elsif new_date[1] == '04'
+        "April #{day}, #{new_date[0]}"
+      elsif new_date[1] == '05'
+        "May #{day}, #{new_date[0]}"
+      elsif new_date[1] == '06'
+        "June #{day}, #{new_date[0]}"
+      elsif new_date[1] == '07'
+        "July #{day}, #{new_date[0]}"
+      elsif new_date[1] == '08'
+        "August #{day}, #{new_date[0]}"
+      elsif new_date[1] == '09'
+        "September #{day}, #{new_date[0]}"
+      elsif new_date[1] == '10'
+        "October #{day}, #{new_date[0]}"
+      elsif new_date[1] == '11'
+        "November #{day}, #{new_date[0]}"
+      elsif new_date[1] == '12'
+        "December #{day}, #{new_date[0]}"
+      end
   end
 
 end
